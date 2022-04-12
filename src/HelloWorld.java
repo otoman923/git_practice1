5e5a36460e993e1327e41d7d997a38032c49af43
otoman923 <mirxat@gmail.com>
2022-04-12 15:34:59 +0300

    work 3 is done

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,7 +1,9 @@
 public class HelloWorld {
     public static void main(String[] args) {
    // do work 2
-
-
+        System.out.println("Hello world");
+        System.out.println("GitHub");
+     // doing awesome work
+        // ready to pushed remote
     }
 }
